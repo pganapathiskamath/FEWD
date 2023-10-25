836b2742d8bb24515d543de22f5c2b967533a4bb
pganapathiskamath <141317945+pganapathiskamath@users.noreply.github.com>
2023-10-26 02:20:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function videoconanimation(){
+var videocon=document.querySelector("#video_container");
+var playbtn=document.querySelector("#play");
+videocon.addEventListener("mouseenter",function(){
+gsap.to(playbtn,{scale:1,opacity:1});
+});
+videocon.addEventListener("mouseleave",function(){
+gsap.to(playbtn,{scale:0,opacity:0});
+});
+videocon.addEventListener("mousemove",function(dets){
+gsap.to(playbtn,{left:dets.x-100,top:dets.y-100});
+});
+}
+videoconanimation();
+function loadinganimation(){
+gsap.from("#page1 h1",{y:100,opacity:0,delay:0.5,duration:0.9,stagger:0.3});
+gsap.from("#page1 #video_container",{scale:0.9,opacity:0,delay:1.3,duration:0.3});
+}
+loadinganimation();
